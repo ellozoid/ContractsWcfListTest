2db50bb255a395c8ee40d521594e096ac04e1726
Danya-png <vaserman267@gmail.com>
2023-03-06 01:00:33 +0400

    Added contract wcf service

---
+++ ContractManagerClient/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Windows;
+
+namespace ContractManagerClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private readonly IContractsService contractsService = new ContractsServiceClient();
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            var contracts = contractsService.GetContracts();
+            foreach (var contract in contracts)
+            {
+                contract.IsRelevant = (DateTime.Now - contract.LastUpdateDate).TotalDays < 60;
+            }
+            ContractsGrid.ItemsSource = contracts;
+        }
+        
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var contract in ContractsGrid.Items)
+            {
+                contractsService.UpdateContract((Contract)contract);
+            }
+        }
+    }
+}
---
+++ ContractsService.Data/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using ContractsService.Data.Models;
+using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace ContractsService.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(string connectionString)
+            : base(connectionString)
+        {
+
+        }
+
+        public virtual DbSet<ContractEntity> Contracts { get; set; }
+    }
+}
